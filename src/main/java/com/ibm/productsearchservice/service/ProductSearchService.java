6904b00d376e95437dd8411aca58d78276192cf7
Mahendra <Mahendra.vasa@ibm.com>
2022-09-09 09:18:38 +0530

    Code Coverage Changes

--- src/main/java/com/ibm/productsearchservice/service/ProductSearchService.java
+++ src/main/java/com/ibm/productsearchservice/service/ProductSearchService.java
@@ -1,13 +1,8 @@
 package com.ibm.productsearchservice.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import com.ibm.productsearchservice.model.ProductDetails;
 
 @Service
 public class ProductSearchService {
